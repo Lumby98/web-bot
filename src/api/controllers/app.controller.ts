7fdcce90990361e9038409e193e55595826fc194
Lumby98 <56871098+Lumby98@users.noreply.github.com>
2021-08-16 14:12:38 +0200

    settings for testing

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import { ScraperService } from '../../core/services/scraper/scraper.service';
 
 @Controller()
@@ -6,10 +6,13 @@ export class AppController {
   constructor(private scraperService: ScraperService) {}
 
   @Get('scrap')
-  async scrap() {
-    /* if (!username || !password) {
+  async scrap(
+    @Query('username') username: string,
+    @Query('password') password: string,
+  ) {
+    if (!username || !password) {
       throw new HttpException('incomplete login information', 400);
-    }*/
+    }
     await this.scraperService.scrap('username', 'password');
   }
 }
--- src/api/test/app.controller.spec.ts
+++ src/api/test/app.controller.spec.ts
@@ -1,22 +1,51 @@
-/*
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from '../controllers/app.controller';
+import { ScraperService } from '../../core/services/scraper/scraper.service';
+
 describe('AppController', () => {
   let appController: AppController;
+  let spyService: ScraperService;
 
   beforeEach(async () => {
+    const serviceProvider = {
+      provide: ScraperService,
+      useFactory: () => ({
+        scrap: jest.fn(),
+      }),
+    };
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [],
+      providers: [serviceProvider],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    spyService = app.get<ScraperService>(ScraperService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.scrap()).toBe('Hello World!');
+  describe('getScrap', () => {
+    it('should pass login check"', () => {
+      const username = 'steve';
+      const password = 'password';
+      appController.scrap(username, password);
+      expect(spyService.scrap).toHaveBeenCalled();
+    });
+  });
+
+  describe('wrongUsername', () => {
+    it('should throw exception "incomplete login information"', () => {
+      const username = '';
+      const password = 'password';
+      appController.scrap(username, password);
+      expect(spyService.scrap).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('wrongPassword', () => {
+    it('should throw exception "incomplete login information"', () => {
+      const username = 'marc';
+      const password = '';
+      appController.scrap(username, password);
+      expect(spyService.scrap).toHaveBeenCalledTimes(0);
     });
   });
 });
-*/

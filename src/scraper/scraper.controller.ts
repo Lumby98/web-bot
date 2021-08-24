9e2401e7ff7e748ba585e6d9eb5ee75b90cc4fc7
Lumby98 <56871098+Lumby98@users.noreply.github.com>
2021-08-24 11:38:30 +0200

    fixed the propblem with not being able to call the controller
    fix the problem by deleting the other reqests in the controller. have a feeling it has something to do with the amount of get requests in the controller

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,46 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-  HttpException,
-} from '@nestjs/common';
+import { Controller, Get, Query, HttpException } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
-import { CreateScraperDto } from './dto/create-scraper.dto';
-import { UpdateScraperDto } from './dto/update-scraper.dto';
 
 @Controller('scraper')
 export class ScraperController {
   constructor(private readonly scraperService: ScraperService) {}
-
-  @Post()
-  create(@Body() createScraperDto: CreateScraperDto) {
-    return this.scraperService.create(createScraperDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.scraperService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.scraperService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateScraperDto: UpdateScraperDto) {
-    return this.scraperService.update(+id, updateScraperDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.scraperService.remove(+id);
-  }
 
   @Get('scrap')
   async scrap(

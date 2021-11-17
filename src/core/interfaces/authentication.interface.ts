a21a2c2d1494e0bfe7737e1162adad0cc92e3c9b
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-11-17 11:27:18 +0100

    List stuff

--- src/core/interfaces/authentication.interface.ts
+++ src/core/interfaces/authentication.interface.ts
@@ -1,7 +1,8 @@
 import { RegisterDto } from '../../ui.api/dto/authentication/register.dto';
 import { UserDto } from '../../ui.api/dto/user/user.dto';
 
-export const authenticationInterfaceProvider = 'authenticationInterfaceProvider';
+export const authenticationInterfaceProvider =
+  'authenticationInterfaceProvider';
 
 export interface AuthenticationInterface {
   register(registrationData: RegisterDto): Promise<UserDto>;

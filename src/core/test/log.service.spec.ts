7ed331767be177c78f2ae99d33ccf8d015021179
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-12-09 15:16:57 +0100

    Test fixed again

--- src/core/test/log.service.spec.ts
+++ src/core/test/log.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogService } from '../service/log/log.service';
+import { getConnectionToken, getRepositoryToken } from '@nestjs/typeorm';
+import { HultaforsProduct } from '../../infrastructure/entities/hultafors.product.entity';
+import { Connection, Repository } from 'typeorm';
+import { LogEntity } from '../../infrastructure/entities/log.entity';
+import { orderInterfaceProvider } from '../interfaces/order.interface';
+import { OrderService } from '../service/log/order.service';
+import { logErrorInterfaceProvider } from '../interfaces/log-error.interface';
+import { LogErrorService } from '../service/log/log-error.service';
+import { logInterfaceProvider } from '../interfaces/log.interface';
+import { OrderEntity } from '../../infrastructure/entities/order.entity';
+import { ErrorEntity } from '../../infrastructure/entities/error.entity';
 
 describe('LogService', () => {
-  let service: LogService;
+  let logService: LogService;
+  let connection;
+  const mockConnection = () => ({
+    transaction: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LogService],
+      providers: [
+        LogService,
+        {
+          provide: Connection,
+          useFactory: mockConnection,
+        },
+        {
+          provide: getRepositoryToken(LogEntity),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(OrderEntity),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(ErrorEntity),
+          useClass: Repository,
+        },
+        { provide: orderInterfaceProvider, useClass: OrderService },
+        { provide: logErrorInterfaceProvider, useClass: LogErrorService },
+      ],
     }).compile();
 
-    service = module.get<LogService>(LogService);
+    logService = await module.get<LogService>(LogService);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(logService).toBeDefined();
   });
 });

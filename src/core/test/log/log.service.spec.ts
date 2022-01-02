fd4aa46ea3a2ca2ca4b6d0915f11ebe1cf6a1cfe
Lumby98 <lumby98@gmail.com>
2022-01-02 11:33:31 +0100

    log test

--- src/core/test/log/log.service.spec.ts
+++ src/core/test/log/log.service.spec.ts
@@ -1,16 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogService } from '../../application.services/implementations/log/log.service';
-import { getConnectionToken, getRepositoryToken } from '@nestjs/typeorm';
-import { HultaforsProduct } from '../../../infrastructure/entities/hultafors.product.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
 import { LogEntity } from '../../../infrastructure/entities/log.entity';
 import { orderInterfaceProvider } from '../../application.services/interfaces/log/order.interface';
 import { OrderService } from '../../application.services/implementations/log/order.service';
 import { logErrorInterfaceProvider } from '../../application.services/interfaces/log/log-error.interface';
 import { LogErrorService } from '../../application.services/implementations/log/log-error.service';
-import { logInterfaceProvider } from '../../application.services/interfaces/log/log.interface';
 import { OrderEntity } from '../../../infrastructure/entities/order.entity';
 import { ErrorEntity } from '../../../infrastructure/entities/error.entity';
+import { CreateLogDto } from '../../../ui.api/dto/log/logEntry/create-log.dto';
+import { ProcessStepEnum } from '../../enums/processStep.enum';
+import { LogModel } from '../../models/logEntry/log.model';
 
 describe('LogService', () => {
   let logService: LogService;
@@ -50,5 +51,46 @@ describe('LogService', () => {
 
   it('should be defined', () => {
     expect(logService).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(OrderService).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(LogErrorService).toBeDefined();
+  });
+
+  describe('create', () => {
+    describe('when given valid data', () => {
+      const dto: CreateLogDto = {
+        status: true,
+        process: ProcessStepEnum.GETORDERINFO,
+        timestamp: new Date(),
+        order: { orderNr: '123', completed: true },
+        error: { errorMessage: 'test' },
+      };
+      const expected: LogModel = {
+        status: true,
+        process: ProcessStepEnum.GETORDERINFO,
+        timestamp: new Date(),
+        order: { orderNr: '123', completed: true, id: 1, logs: [] },
+        error: {
+          errorMessage: 'test',
+          displayErrorMessage: undefined,
+          id: 1,
+          logs: [],
+        },
+        id: 1,
+      };
+      let result;
+      beforeEach(async () => {
+        result = await logService.create(dto);
+      });
+
+      it('should return logModel', () => {
+        expect(result).toEqual(expected);
+      });
+    });
   });
 });
